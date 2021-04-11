503ae7ff4ae07f34ee1be9a5273f3d59f4f66234
ion-project <gabrielcursino.medeiros@gmail.com>
2021-04-10 22:02:21 -0300

    fix(): Improve find input

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
 import { Mask } from '@ionited/mask';
 import { Directive } from 'vue';
 
+function addMask(el: HTMLElement, input: HTMLInputElement, options: any) {
+  (el as any).mask = Mask(input, options);
+}
+
 export const mask: Directive = {
   mounted(el: HTMLElement, binding) {
     let input: HTMLInputElement;
 
     if (el.nodeName === 'INPUT') {
       input = el as HTMLInputElement;
+
+      addMask(el, input, binding.value);
     } else {
       input = el.querySelector('input') as HTMLInputElement;
-    }
 
-    (el as any).mask = Mask(input, binding.value);
+      if (input) {
+        addMask(el, input, binding.value);
+      } else if ((el as any).getInputElement) {
+        (el as any).getInputElement()
+        .then((input: HTMLInputElement) => addMask(el, input, binding.value));
+      }
+    }
   },
 
   unmounted(el: any) {
